60735367a97348f0c9cb38dad480b89ee53de337
Floki212 <89355174+Floki212@users.noreply.github.com>
2021-10-01 16:23:09 +0200

    Update project1.py

--- project1.py
+++ project1.py
@@ -3,12 +3,30 @@ from bs4 import BeautifulSoup
 import csv
 import urllib3
 
-    #Every page and books link to scrape
+#premier livre a scrape
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 response = requests.get(url)
-#soup = BeautifulSoup(url)
 soup = BeautifulSoup(response.content, "html.parser")
-links = soup.find_all("td")
-#for link in soup.find_all("a"):
-print(links)   
+
+# Link du livre
+
+links = []
+link = soup.find("article")
+for a in soup.find("a", href=True):
+    links.append(a["href"])
+
+# Scraper le titre du livre
+titles = []
+title = soup.find("article")
+for article in title:
+    titles = article.find("h1")
+    print(titles)
+
+# Scraper les informations (UPC, prix, stock ...)
+
+upc = soup.find("td")
+print(upc)
+
+product = soup.find("td")
+print(product)
 
