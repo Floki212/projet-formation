3b44aaebb58e9ed21d9a31a93e6e9e74640b2bc2
Floki212 <89355174+Floki212@users.noreply.github.com>
2021-10-13 13:47:33 +0200

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,32 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import urllib3
 
 #premier livre a scrape
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
+#if response.status_code -- 200:
+#else:
+#    print("erreur de connexion")
 
 # Link du livre
 
-links = []
-link = soup.find("article")
-for a in soup.find("a", href=True):
-    links.append(a["href"])
+#links = []
+#link = soup.find("article")
+#for a in soup.find("a", href=True):
+#    links.append(a["href"])
 
 # Scraper le titre du livre
 titles = []
-title = soup.find("article")
+title = soup.find("article", class_="product_page")
 for article in title:
     titles = article.find("h1")
     print(titles)
 
 # Scraper les informations (UPC, prix, stock ...)
+info = []
+info = soup.findAll("td")
+print(info)
 
-upc = soup.find("td")
-print(upc)
+# Scraper la description du livre
 
-product = soup.find("td")
-print(product)
+descriptions = []
+description = soup.find("article", class_="product_page")
+for desc in description:
+    description = desc.find("p", h2=True)
+    print(description)
 
+#if response.status_code -- 200:
+#    else:
+#        print("erreur de connexion")
+
+book = {
+        "product_type": info[1],
+        "price_excl_tax": info[2],
+        "price_incl_tax": info[3],
+        "number_available": info[5],
+        "number_review": info[6],
+}
+print(book)
